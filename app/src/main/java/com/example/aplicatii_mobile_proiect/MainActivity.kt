302806da4e14277a23cf8969782356861ff53b1f
U-OptimusJR\Nicolaegis <nicolae.vartolomei@gmail.com>
2020-04-05 16:14:37 +0300

    Initial commit of the project

---
+++ app/src/main/java/com/example/aplicatii_mobile_proiect/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.aplicatii_mobile_proiect
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
